63602a3c0dab34cc33714e24307cb3b392fbf207
Ben Miller <bsmiller25@gmail.com>
2020-05-17 14:47:28 -0400

    fix staticfiles

--- bank1756/settings.py
+++ bank1756/settings.py
@@ -124,8 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "bank1756/static"),
+    os.path.join(BASE_DIR, "bank1756/static/"),
  )
 
 LOGIN_URL = '/admin/'
